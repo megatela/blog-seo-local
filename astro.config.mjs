f66bf0732fd135485fcd220c515a1717a5e8fd48
megatela <carmenestelasi@gmail.com>
2026-01-26 05:55:59 -0400

    Activar integración de React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import sanity from "@sanity/astro";
-
+import react from "@astrojs/react";
 export default defineConfig({
   integrations: [
     sanity({
