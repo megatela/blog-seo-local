d3e29e002ae58c8f8ba0c0f98d1485102f9082df
megatela <carmenestelasi@gmail.com>
2026-01-26 06:11:58 -0400

    Forzando instalacion de React en Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
+
 export default defineConfig({
   integrations: [
     sanity({
-      projectId: "qv6q15su", // Tu ID de proyecto
+      projectId: "qv6q15su",
       dataset: "production",
       useCdn: true,
-      stega: {
-        enabled: false,
-      },
+      stega: { enabled: false },
     }),
+    react(),
   ],
 });
