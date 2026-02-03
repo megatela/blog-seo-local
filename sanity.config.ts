269329f19efb16b2c7a93ef325f93698b1fe11f5
megatela <carmenestelasi@gmail.com>
2026-02-03 08:23:13 -0400

    Fix: Añadiendo styled-components para el build de Sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,15 +5,18 @@ import { schemaTypes } from './studio/schemaTypes'
 export default defineConfig({
   name: 'default',
   title: 'Local SEO Architect Admin',
-
   projectId: 'qv6q15su', 
   dataset: 'production',
-
   plugins: [
     deskTool(), 
   ],
-
   schema: {
     types: schemaTypes,
   },
+  // Añadimos esto para que Vercel no se queje de las librerías visuales
+  form: {
+    components: {
+      input: (props) => props.renderDefault(props),
+    }
+  }
 })
