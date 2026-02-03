5615fe0275e9f931ce5b872d931d19e53914a30c
megatela <carmenestelasi@gmail.com>
2026-02-03 07:43:05 -0400

    Fix: Activando ruta /studio con integracion oficial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
-import sanity from "@sanity/astro";
-import react from "@astrojs/react";
+import sanity from '@sanity/astro';
+import react from '@astrojs/react'; // El panel de Sanity necesita React
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [
-    sanity({
-      projectId: "qv6q15su",
-      dataset: "production",
-      useCdn: true,
-      stega: { enabled: false },
-    }),
     react(),
+    sanity({
+      projectId: 'qv6q15su',
+      dataset: 'production',
+      studioBasePath: '/studio', // Esto crea la URL /studio automáticamente
+    }),
   ],
 });
