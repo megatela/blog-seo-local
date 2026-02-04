60d1f60d38694286ec17fbdf87e4580cbf6df7c9
megatela <carmenestelasi@gmail.com>
2026-02-04 07:59:09 -0400

    fix: forzando prerenderizado estatico

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,16 +1,14 @@
-// src/lib/sanity.ts
-import { createClient } from '@sanity/client'
-
-export const client = createClient({
-  projectId: 'qv6q15su', // Tu ID real
-  dataset: 'production',
-  useCdn: false, // 'false' para ver cambios al instante
-  apiVersion: '2024-01-24', 
-})
+import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-const builder = imageUrlBuilder(client);
+export const client = createClient({
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
+  dataset: import.meta.env.PUBLIC_SANITY_DATASET || 'production',
+  useCdn: true,
+  apiVersion: '2023-05-03',
+});
 
+const builder = imageUrlBuilder(client);
 export function urlFor(source: any) {
   return builder.image(source);
 }
